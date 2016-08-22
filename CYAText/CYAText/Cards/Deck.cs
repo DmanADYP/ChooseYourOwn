1261265d95fb9db3835d0300e8a66fafe243fb10
cabbott91 <cabbott@virtusa.com>
2016-08-22 08:45:21 -0400

    Added DrawCard, and Shuffle

---
+++ CYAText/CYAText/Cards/Deck.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CYAText.CYAEngine.BaseItems;
+
+namespace CYAText.Cards
+{
+    class Deck
+    {
+        private List<BaseCard> deck = new List<BaseCard>();
+        private List<BaseCard> discardDeck = new List<BaseCard>();
+        private Random rand;
+
+
+        public Deck(Random rand) {
+            this.rand = rand;
+            //TODO: Get the deck of cards from Database
+        }
+
+        public void Shuffle()
+        {
+            List<BaseCard> tempDeck = new List<BaseCard>();
+            for (int index = 0; index < deck.Count; index++)
+            {
+                tempDeck.Add(deck.ElementAt(rand.Next(0,deck.Count-1)));
+            }
+            deck = tempDeck;
+        }
+
+        public BaseCard DrawCard()
+        {
+            BaseCard card;
+            if (deck.Count != 0) { 
+                card = deck.First();
+                deck.RemoveAt(0);
+                discardDeck.Add(card);
+            }
+            else
+            {
+                deck = discardDeck;
+                this.Shuffle();
+                return this.DrawCard();
+            }
+            
+            return card;
+        }
+    }
+}
